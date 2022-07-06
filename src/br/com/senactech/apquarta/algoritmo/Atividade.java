d9c8e61b5fd4e65f267565bd0cc29c3cef357f1a
João Vitor Floriano Barbosa <182110056@senacinformatica.br>
2022-07-06 14:20:34 -0300

    refatorando o projeto e suas dependências

---
+++ src/br/com/senactech/apquarta/algoritmo/Atividade.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.senactech.apquarta.algoritmo;
+
+/**
+ *
+ * @author 182110056
+ */
+public class Atividade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
